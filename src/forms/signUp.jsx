7eaca649c0e1add808dfd7d4baebee4701fba530
ShaoorMazhar <94368157+ShaoorMazhar@users.noreply.github.com>
2022-08-08 03:16:26 -0700

    console removed

--- src/forms/signUp.jsx
+++ src/forms/signUp.jsx
@@ -30,7 +30,6 @@ export default function SignUp() {
     };
     const userData = await signUp(newUser);
     dispatch(sign_up(userData));
-    console.log(userData, "ddd");
     if (userData?.metadata?.status === "SUCCESS") {
       toast("Sign Up Successfully!");
       setUserName(""), setEmail(""), setPassword("");
